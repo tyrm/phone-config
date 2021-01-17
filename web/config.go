f9cb820a0011e2463849563669fc3617ff23bc19
Tyr Mactire <tyr@pettingzoo.co>
2021-01-17 06:49:31 -0800

    updates

--- web/config.go
+++ web/config.go
@@ -20,8 +20,17 @@ func GetConfig(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Get Phone
+
 	vars := mux.Vars(r)
+	mode := "phone"
+
+	switch vars["mac"] {
+	case "F0V0X5U00000":
+		mode = "generic"
+	}
+
+	// Get Phone
+
 	phone, err := models.GetPhone(vars["mac"], vars["vendor"], vars["model"])
 	if err != nil {
 		msg := fmt.Sprintf("could not get phone: %s", err.Error())
@@ -29,7 +38,8 @@ func GetConfig(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, msg, http.StatusUnauthorized)
 		return
 	}
-	if phone == nil {
+
+	if phone == nil && mode == "phone" {
 		http.Error(w, "phone not found", http.StatusNotFound)
 		return
 	}
@@ -38,15 +48,19 @@ func GetConfig(w http.ResponseWriter, r *http.Request) {
 	tmplVars := ConfigTemplate{}
 	tmplVars.ProvisioningServer = extHostname
 
-	tmplVars.Lines, err = phone.GetLines()
-	if err != nil {
-		msg := fmt.Sprintf("could not get lines: %s", err.Error())
-		logger.Errorf(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
-		return
+	if mode == "phone" {
+		tmplVars.Lines, err = phone.GetLines()
+		if err != nil {
+			msg := fmt.Sprintf("could not get lines: %s", err.Error())
+			logger.Errorf(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
+		}
 	}
 
-	templateName := fmt.Sprintf("%s_%s_%s", vars["vendor"], vars["model"], vars["suffix"])
+	// Select Template
+	templateName := fmt.Sprintf("%s_%s_%s_%s", vars["vendor"], vars["model"], vars["suffix"], mode)
+
 	err = templates.ExecuteTemplate(w, templateName, tmplVars)
 	if err != nil {
 		msg := fmt.Sprintf("could not render template: %s", err.Error())
